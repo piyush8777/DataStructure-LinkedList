d2be65395a77ae9fabb514c03e7b6f60a8ee3eec
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-14 15:26:49 +0530

    Added : Ability to create list by appending 30 and 70 to 56

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,4 +1,45 @@
 
 public class MyLinkedList {
+	 public INode head;
+	    public INode tail;
 
+	    public MyLinkdList(){
+	        this.head = head ;
+	        this.tail = tail;
+	    }
+
+	    public void add(INode newNode) {
+	        if(this.head == null)
+	            this.head = newNode;
+	        if(this.tail == null)
+	            this.tail = newNode;
+	        else{
+	            INode tempNode = this.head;
+	            this.head = newNode;
+	            this.head.setNext(tempNode);
+	        }
+	    }
+
+	    public void printMyNodes(){
+	        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+	        INode tempNode = head ;
+	        while (tempNode.getNext() != null){
+	            myNodes.append(tempNode.getKey());
+	            if(!tempNode.equals(tail))
+	                myNodes.append("->");
+	            tempNode = tempNode.getNext();
+	        }
+	        myNodes.append(tempNode.getKey());
+	        System.out.println(myNodes);
+	    }
+	    public void append(INode newNode) {
+	        if(this.head == null)
+	            this.head = newNode;
+	        if(this.tail == null)
+	            this.tail = newNode;
+	        else{
+	            this.tail.setNext(newNode);
+	            this.tail = newNode;
+	        }
+	    }
 }
